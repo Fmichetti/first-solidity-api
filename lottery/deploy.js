dadfb1d0cd31624efc156e79157801495dd1a46e
Fernando <fmichetti@crosscommercestore.com>
2022-03-15 18:17:17 -0300

    Lottery with frontend

---
+++ lottery/deploy.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3'); // uppercase, because it is a constructor
+const { interface, bytecode } = require('./compile');
+
+// specify which accout will be used to pay for contract deployment and
+// witch network will be deployed
+const provider = new HDWalletProvider(
+    'shoot laptop north stage bulb dumb desert strategy knife expand dismiss chief',
+    'https://rinkeby.infura.io/v3/8a3093529c194b66bd9300f4c23c7b8e'
+);
+
+//instance of web3
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account:', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log(interface);
+    console.log('Contract deployed to:', result.options.address);
+    
+    provider.engine.stop();
+};
+
+deploy();
---
+++ lottery/test/Lottery.test.js
@@ -0,0 +1,133 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+require('dotenv').config();
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+
+describe('Lottery Contract', () => {
+    it('deploys a contract', () => {
+        assert.ok(lottery.options.address);
+    });
+
+    it('allows one account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        })
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
+    });
+
+    it('allows multiple accounts to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        })
+
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether')
+        })
+
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether')
+        })
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length);
+    });
+
+    it('requires a minimum amount of ether to enter', async () => {
+        let executed;
+
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei('0', 'ether')
+            })
+            executed = 'success';
+        } catch (error) {
+            executed = 'fail'
+        }
+
+        assert.equal('fail', executed);
+    });
+
+    it('only manager can call pickWinner', async () => {
+        let executed_not_manager;
+        let executed_manager;
+
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        })
+
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[0] //manager account
+            })
+            executed_manager = 'success';
+        } catch (error) {
+            executed_manager = 'fail';
+        }
+
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[1] // not a manager account
+            })
+            executed_not_manager = 'success';
+        } catch (error) {
+            executed_not_manager = 'fail';
+        }
+
+
+
+        assert.equal('success', executed_manager);
+        assert.equal('fail', executed_not_manager);
+    });
+
+    it('sends money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert(difference > web3.utils.toWei('1.999', 'ether'));
+        assert(players.length == 0);
+    });
+
+});
